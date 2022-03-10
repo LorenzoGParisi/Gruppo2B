60a00243216cd26df2f2cfa2901c18f73a82b8ac
LorenzoGParisi <lorenzo.parisi@edu.itspiemonte.it>
2022-03-10 12:00:03 +0100

    Pubblicato

---
+++ GitHubJavaProgetto2/src/hello/HelloWorld.java
@@ -0,0 +1,25 @@
+package hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		lorenzoParisi();
+		marcelloBorio();
+		leonardoCionchi();
+		matteoSquarotti();
+		matteoMezzanotte();
+		marcoAstolfi();
+	}
+	public static void lorenzoParisi(){
+	}
+	public static void marcelloBorio(){
+	}
+	public static void leonardoCionchi(){
+	}
+	public static void matteoSquarotti(){
+	}
+	public static void matteoMezzanotte(){
+	}
+	public static void marcoAstolfi(){
+	}
+}
